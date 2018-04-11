af5be6ce75ca7a770a557425c42449c31c52625e
калиевар <@227_13>
2018-04-11 09:22:45 +0600

    Add project files.

---
+++ ConsoleApplication3/Settings.h
@@ -0,0 +1,10 @@
+#include "Students.h"
+
+void CreateName(char *a);
+void generateDate(dates * date);
+Gender genGender();
+void genSub(subject * sub);
+void printStudents(students * stud);
+void getCharGender(Gender m);  
+void printStudents(students * stud);
+students * genStud(int * randStud);
---
+++ ConsoleApplication3/Source.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <Windows.h>
+#include <iostream>
+#include <string.h>
+#include <time.h>
+#include "Settings.h"
+#include "Studentswork.h"
+
+void main()
+{
+
+	CreatFileOfStudents("stud.txt", "w");
+}
+
